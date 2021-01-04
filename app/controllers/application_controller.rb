9736724365673711cbf5fe6cf445d78fb9ec15b6
hidetsugu-o <osyame@gmail.com>
2021-01-04 12:09:12 +0900

    ユーザーモデルの単体テストコードを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -8,5 +7,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
-
 end
