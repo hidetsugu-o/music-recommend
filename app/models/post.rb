87afd6976e3b212ac11b762a893a62adbcbb8fe5
hidetsugu-o <osyame@gmail.com>
2021-01-04 20:43:09 +0900

    投稿と一覧表示機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :link, presence: true
+
+  def self.get_id
+    posts = Post.order('created_at DESC')
+    ids = []
+    
+    posts.each do |post|
+      url = post.link
+      if url.include?("=")
+        url.slice!("https://www.youtube.com/watch?v=")
+      else
+        url.slice!("https://youtu.be/")
+      end
+      ids.push(url)
+    end
+
+    return ids
+  end
 end
