85955eea2d7fb9587fcb5d34ec1a56b588e53106
hidetsugu-o <osyame@gmail.com>
2021-01-07 19:07:38 +0900

    非同期通信を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
     like = Like.create(like_params)
-    redirect_to root_path
   end
 
   def destroy
     like = Like.find_by(like_params)
     like.destroy
-    redirect_to root_path
   end
 
   private
   def like_params
     params.permit(:post_id).merge(user_id: current_user.user_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
