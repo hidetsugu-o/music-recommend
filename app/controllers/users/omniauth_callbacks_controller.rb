05d02bcb7fc12f327eeff461fd657a3bcd8863a9
hidetsugu-o <osyame@gmail.com>
2021-01-14 11:46:51 +0900

    Rubocopの実行

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
   def line
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentization
     else
       redirect_to root_path
     end
-
   end
 end
