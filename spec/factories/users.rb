b92f318e3515649d5d7be7fc756d48a607fb2837
hidetsugu-o <osyame@gmail.com>
2021-01-15 18:11:50 +0900

    User関連の結合テストコードを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     user_id               { Faker::Internet.password(min_length: 20) + '1a' }
     name                  { Faker::Name.name }
     icon                  { Faker::Internet.url }
-    message               { 'メッセージ' }
+    message               { Faker::Music.instrument }
     password = Faker::Internet.password(min_length: 6)
     password { password }
     password_confirmation { password }
